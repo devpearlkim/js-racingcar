71d38836dfda5b3a1796aa423e50f0368f543178
JINJU KIM <devpearlkim@gmail.com>
2025-02-09 18:05:14 +0900

    docs: Add requirements for the car racing game
    - Define car name input rules
    - Specify race process
    - Add conditions for invalid input handling

---
+++ src/game.js
@@ -0,0 +1,31 @@
+import readline from 'readline';
+
+function readLineAsync(query) {
+  return new Promise((resolve, reject) => {
+    if (arguments.length !== 1) {
+      reject(new Error('arguments must be 1'));
+    }
+
+    if (typeof query !== 'string') {
+      reject(new Error('query must be string'));
+    }
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question(query, (input) => {
+      rl.close();
+      resolve(input);
+    });
+  });
+}
+
+// 입출력 예시
+async function play() {
+  const name = await readLineAsync('자동차 이름을 입력하세요 > ');
+  console.log(`자동차 이름은 ${name}입니다.`);
+}
+
+play();
